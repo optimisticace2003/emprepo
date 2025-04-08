44baf7109887e636bc2fd49ab79a07877302712c
optimisticace2003 <manikrakshan16@gmail.com>
2025-04-08 12:30:12 +0530

    exceptions

---
+++ EmpJDBCProject/src/com/hexaware/exception/ProjectExceptions.java
@@ -0,0 +1,32 @@
+package com.hexaware.exception;
+
+public class ProjectExceptions extends Exception{
+
+	public ProjectExceptions() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProjectExceptions(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProjectExceptions(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProjectExceptions(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ProjectExceptions(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
