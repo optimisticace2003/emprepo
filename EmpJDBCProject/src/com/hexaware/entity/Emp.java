eebdcf3fa16c10b3514a6d5006f0ac1c91278b5a
optimisticace2003 <manikrakshan16@gmail.com>
2025-04-08 13:07:07 +0530

    adding tostring method

--- EmpJDBCProject/src/com/hexaware/entity/Emp.java
+++ EmpJDBCProject/src/com/hexaware/entity/Emp.java
@@ -1,7 +1,15 @@
 package com.hexaware.entity;
 
 public class Emp {
+	public static int empId =9;
     public static void main(String[]args){
         System.out.println("Employee");
     }
+
+	@Override
+	public String toString() {
+		return "EmpId = " + empId;
+	}
+    
+    
 }
